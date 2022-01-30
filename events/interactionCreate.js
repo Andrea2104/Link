03fc74cd7d6c0ce1a3736b7e226a393c6237464b
Andrea <andysmith.mememan@gmail.com>
2022-01-30 03:59:28 -0800

    reverted

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,7 +4,7 @@ client.on("interactionCreate", async (interaction) => {
     // Slash Command Handling
     
     if (interaction.isCommand()) {
-        await interaction.deferReply({ ephemeral: true }).catch(() => {});
+        await interaction.deferReply({ ephemeral: false }).catch(() => {});
 
         const cmd = client.slashCommands.get(interaction.commandName);
         if (!cmd)
@@ -27,7 +27,7 @@ client.on("interactionCreate", async (interaction) => {
 
     // Context Menu Handling
     if (interaction.isContextMenu()) {
-        await interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply({ ephemeral: false });
         const command = client.slashCommands.get(interaction.commandName);
         if (command) command.execute(client, interaction);
     }
